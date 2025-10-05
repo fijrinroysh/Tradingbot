70c286603b10ccae7dbe5158039db820e034331a
fijrinroysh <52443135+fijrinroysh@users.noreply.github.com>
2025-10-05 15:01:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,17 @@
-from flask import Flask, request
-import requests
+    from flask import Flask
 
-app = Flask(__name__)
+    app = Flask(__name__)
 
-ALPACA_API_KEY = 'PKWMQDBXMMYIUO61F9X4'
-ALPACA_SECRET_KEY = 'KYfU5eXz4oMhcTdwouYEiDKfKElOoW2S034I4tSU'
-BASE_URL = 'https://paper-api.alpaca.markets'
+    @app.route('/')
+    def index():
+        return '<h1>Hello World!</h1>'
 
-
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    data = request.json
-    symbol = data['symbol']
-    qty = data['qty']
-    side = data['action']
-
-    order = {
-        "symbol": symbol,
-        "qty": qty,
-        "side": side,
-        "type": "market",
-        "time_in_force": "gtc"
-    }
-
-    r = requests.post(f"{BASE_URL}/v2/orders", json=order, headers={
-        "APCA-API-KEY-ID": ALPACA_API_KEY,
-        "APCA-API-SECRET-KEY": ALPACA_SECRET_KEY
-    })
+    if __name__ == "__main__":
+        app.run(debug=True) # Set debug to True for development
 
 
-    return {"status": "order sent", "response": r.json()}
+
+
 
 
 
