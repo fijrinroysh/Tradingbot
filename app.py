2db40ba741eed650f5702b0f52d445bff1c7a695
fijrinroysh <52443135+fijrinroysh@users.noreply.github.com>
2025-10-05 15:09:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
-    from flask import Flask
+from flask import Flask
 
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route('/')
-    def index():
-        return '<h1>Hello World!</h1>'
-
-    if __name__ == "__main__":
-        app.run(debug=True) # Set debug to True for development
-
-
-
-
-
-
-
+@app.route('/')
+def index():
+	return '<h1>Hello World!</h1>'
+if __name__ == "__main__":
+	app.run(debug=True) # Set debug to True for development
